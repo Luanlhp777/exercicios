f490ae0d1a44bd0673446483e1ef4a271fd44a91
Luanlhp777 <pereira.luan@gmail.com>
2026-04-23 19:40:32 -0300

    Adiciona página html para exercicio2

--- exercicio2.js
+++ exercicio2.js
@@ -2,10 +2,10 @@ const prompt = require("prompt-sync")();
 
 // Programa que leia o preço de um produto e o percentual de desconto oferecido, e calcule o preço final com o desconto aplicado.
 
-let precoFeijao = Number(prompt("Preço do produto: "));
+let preco = Number(prompt("Preço do produto: "));
 let percentualDesconto = Number(prompt("Percentual de desconto: "));
 // Formula para calcular o desconto é: desconto = (preço * percentual) / 100
-let desconto = (precoFeijao * percentualDesconto) / 100;
+let desconto = (preco * percentualDesconto) / 100;
 // Formula para calcular o valor final: preçoFinal = preço - desconto
-let precoFinal = precoFeijao - desconto;
+let precoFinal = preco - desconto;
 console.log(`O produto vai custar ${precoFinal}`);
